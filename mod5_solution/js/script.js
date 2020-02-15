5044472bb5db11572633e1a6af37fc82711d7f14
atatak <59841953+atatak@users.noreply.github.com>
2020-02-15 23:09:35 +0100

    aha

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       
-      var chosenCategoryShortName = chooseRandomCategory(categories); //.short_name
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
                            "randomCategoryShortName",
-                           "'" + category.short_name + "'");
+                           "'" + chosenCategoryShortName + "'");
         
        //insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
